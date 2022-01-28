65d13abf25213d007e767a5be7aa8a3ec71085f2
wilsonwoodyard <98561460+wilsonwoodyard@users.noreply.github.com>
2022-01-28 14:18:01 -0500

    Add files via upload

---
+++ ByteCoin-iOS13/ByteCoin/Model/CoinData.swift
@@ -0,0 +1,13 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by Wilson Woodyard on 1/25/22.
+//  Copyright © 2022 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CoinData: Codable {
+    let rate: Double
+}
